495495aff280d1100fe18dbb0962b51e001a2c98
donan2k5 <tuphucnguyen20051@gmail.com>
2026-06-14 17:15:35 +0700

    fix: use DaoAuthenticationProvider constructor injection to resolve deprecation

--- backend/src/main/java/com/sba301/backend/config/AuthConfig.java
+++ backend/src/main/java/com/sba301/backend/config/AuthConfig.java
@@ -24,8 +24,7 @@ public class AuthConfig {
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
